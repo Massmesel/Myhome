3f0ca597b4ebe671f6b376c86e315660475c7ed4
Massmesel <redwanzeynu14@gmail.com>
2026-06-28 18:48:32 +0300

    Create AuthRepository.kt

---
+++ app/src/main/java/com/habesha/realestate/auth/AuthRepository.kt
@@ -0,0 +1,12 @@
+package com.habesha.realestate.auth
+
+class AuthRepository {
+    // Authentication logic will be implemented here
+    fun loginUser() {
+        // TODO: Implement login
+    }
+    
+    fun registerUser() {
+        // TODO: Implement registration
+    }
+}
